29e3b55bb4d9aba37c991ba1917ef9a9a40778a1
mariamkhmahran <mariamkhmahran@hotmail.com>
2018-07-16 13:36:05 +0200

    added charts route

--- src/components/layout/App/App.js
+++ src/components/layout/App/App.js
@@ -3,31 +3,42 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Focus from "src/components/screens/Focus";
 import Tasks from "src/components/screens/Tasks";
+import Charts from "src/components/screens/Charts/Charts";
+import Header from "src/components/layout/Header";
 import TimerContext, { Context } from "src/logic/TimerContext";
+import TasksContext, { ContextTasks } from "src/logic/TasksContext";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <TimerContext>
-          <Context.Consumer>
-            {({ isTimerRunning }) => (
-              <div className={`App ${isTimerRunning ? "ON" : ""}`}>
-                <Switch>
-                  <Route exact path="/" component={Focus} />
-                  <Route exact path="/tasks" component={Tasks} />
-                </Switch>
-              </div>
+        <TasksContext>
+          <ContextTasks.Consumer>
+            {({ addCycle }) => (
+              <TimerContext addCycle={addCycle}>
+                <Context.Consumer>
+                  {({ isTimerRunning }) => (
+                    <div className={`App ${isTimerRunning ? "ON" : ""}`}>
+                      <Header />
+                      <Switch>
+                        <Route exact path="/" component={Focus} />
+                        <Route exact path="/tasks" component={Tasks} />
+                        <Route exact path="/charts" component={Charts} />
+                      </Switch>
+                    </div>
+                  )}
+                </Context.Consumer>
+              </TimerContext>
             )}
-          </Context.Consumer>
-        </TimerContext>
+          </ContextTasks.Consumer>
+        </TasksContext>
       </BrowserRouter>
     );
   }
 }
-class UpdateBlocker extends React.PureComponent {
-  render() {
-    return this.props.children;
-  }
-}
+// class UpdateBlocker extends React.PureComponent {
+//   render() {
+//     return this.props.children;
+//   }
+// }
 export default App;
