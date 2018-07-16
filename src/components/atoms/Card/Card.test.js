95e35caeec7231af03efc346d52da0379a0c52ba
mariamkhmahran <mariamkhmahran@hotmail.com>
2018-07-16 14:10:48 +0200

    added card files into a seperate file

---
+++ src/components/atoms/Card/Card.test.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { mount } from "enzyme";
+import Card from "src/components/atoms/Card";
+
+test("TasksCard", () => {});
---
+++ src/components/atoms/Card/index.js
@@ -0,0 +1,1 @@
+export { QueueCard, TaskCard } from "./Card";
