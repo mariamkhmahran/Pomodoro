fd9afe67f97ff86e5cb2e7af81bdd6e8a3cb82c4
mariamkhmahran <mariamkhmahran@hotmail.com>
2018-07-16 13:42:09 +0200

    added all clock functionalities and needed logic

--- src/components/screens/Focus/Clock.jsx
+++ src/components/screens/Focus/Clock.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import moment from "moment";
 import { Context } from "src/logic/TimerContext";
+import play_btn from "src/pics/play_btn.png";
+import pause_btn from "src/pics/pause_btn.png";
 
 const Clock = () => (
-  <div>
+  <div className="left_col">
     <Context.Consumer>
-      {({ remainigTime }) => (
-        <h1 id="clock">{moment(remainigTime).format("mm:ss")}</h1>
+      {({ isTimerRunning, stopTimer, startTimer, remainigTime }) => (
+        <div>
+          <h1 id="clock">{moment(remainigTime).format("mm:ss")}</h1>
+          <button
+            className="no-btn-styles round"
+            onClick={() => (isTimerRunning ? stopTimer() : startTimer())}>
+            {isTimerRunning ? (
+              <img className="circular_btn" src={pause_btn} alt="pause" />
+            ) : (
+              <img className="circular_btn" id="play" src={play_btn} alt="start" />
+            )}
+          </button>
+        </div>
       )}
     </Context.Consumer>
   </div>
