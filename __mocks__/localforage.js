e6efd0c51c33ce6b8d9ed06d2d693d77f55242a4
mariamkhmahran <mariamkhmahran@hotmail.com>
2018-07-26 12:32:15 +0200

    fixed object cloning to storage process

--- __mocks__/localforage.js
+++ __mocks__/localforage.js
@@ -2,12 +2,26 @@ export const storage = {};
 
 export default {
   async getItem(key) {
+    let value;
     await wait(10);
-    return storage[key];
+    if (storage[key]) {
+      value = Array.isArray(storage[key])
+        ? storage[key]
+        : JSON.parse(storage[key]);
+    }
+    return value ? value : null;
   },
   async setItem(key, value) {
     await wait(10);
-    storage[key] = value;
+    let temp;
+    if (Array.isArray(value)) {
+      temp = value.slice();
+    } else if (typeof value === "object") {
+      temp = JSON.stringify(value);
+    } else {
+      temp = value;
+    }
+    storage[key] = temp;
   },
   async removeItem(key) {
     await wait(10);
