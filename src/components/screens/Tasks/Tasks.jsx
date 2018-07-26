ee8d51f30731d8cc3bd083321d0099e4f1483517
mariamkhmahran <mariamkhmahran@hotmail.com>
2018-07-26 12:45:16 +0200

    added modal opening and closing controls

--- src/components/screens/Tasks/Tasks.jsx
+++ src/components/screens/Tasks/Tasks.jsx
@@ -4,51 +4,67 @@ import { TaskCard } from "src/components/atoms/Card";
 import { ContextTasks } from "src/logic/TasksContext";
 
 export default class Tasks extends React.Component {
-  state = { ModalOpen: false };
+  state = {
+    addTaskModalIsOpen: false
+  };
+
   render() {
+    const { addTaskModalIsOpen } = this.state;
     return (
       <ContextTasks.Consumer>
-        {({ tasks, done, addTask, addCycle, markAsDone }) => (
-          <div className="router_output">
-            <Modal addTask={addTask} open={this.state.ModalOpen} />
-            <div className="subHeader">
-              <span className="subtitle">TASKS</span>
-              <button
-                className="btn plusBtn"
-                onClick={() =>
-                  this.setState({ ModalOpen: true }, () =>
-                    this.setState({ ModalOpen: false })
-                  )
-                }>
-                <i className="fa fa-plus" />
-              </button>
-            </div>
-            <div className="Main tasks">
-              <div>
-                <div className="col">
-                  <div className="transparent_box">
-                    <span className="subtitle">TO DO</span>
-                    <div className="cardList">
-                      {tasks.map(task => (
-                        <TaskCard key={task.id} task={task} onclick={markAsDone} />
-                      ))}
+        {({ tasks, done, addTask, addCycle, markAsDone, deleteTask }) => {
+          return (
+            <div className="router_output">
+              <Modal
+                addTask={addTask}
+                isOpen={addTaskModalIsOpen}
+                onClose={() => this.setState({ addTaskModalIsOpen: false })}
+              />
+              <div className="subHeader">
+                <span className="subtitle">TASKS</span>
+                <button
+                  className="btn plusBtn"
+                  onClick={() => this.setState({ addTaskModalIsOpen: true })}>
+                  <i className="fa fa-plus" />
+                </button>
+              </div>
+              <div className="Main tasks">
+                <div>
+                  <div className="col">
+                    <div className="transparent_box">
+                      <span className="subtitle">TO DO</span>
+                      <div className="cardList">
+                        {tasks.map(task => (
+                          <TaskCard
+                            key={task.id}
+                            task={task}
+                            onclick={markAsDone}
+                            onDelete={deleteTask}
+                          />
+                        ))}
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div className="col">
-                  <div className="transparent_box">
-                    <span className="subtitle">DONE</span>
-                    <div className="cardList">
-                      {done.map(task => (
-                        <TaskCard key={task.id} task={task} onclick={markAsDone} />
-                      ))}
+                  <div className="col">
+                    <div className="transparent_box">
+                      <span className="subtitle">DONE</span>
+                      <div className="cardList">
+                        {done.map(task => (
+                          <TaskCard
+                            key={task.id}
+                            task={task}
+                            onclick={markAsDone}
+                            onDelete={deleteTask}
+                          />
+                        ))}
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        )}
+          );
+        }}
       </ContextTasks.Consumer>
     );
   }
