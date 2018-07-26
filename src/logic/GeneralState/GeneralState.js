35a1ac688ae65779a45cc0409b0dff8c7a61bec2
mariamkhmahran <mariamkhmahran@hotmail.com>
2018-07-26 12:46:04 +0200

    added chartData manipulation functions

--- src/logic/GeneralState/GeneralState.js
+++ src/logic/GeneralState/GeneralState.js
@@ -1,4 +1,5 @@
 import localforage from "localforage";
+import chartData from "src/mocks/ChartData.json";
 
 async function set(attr, value, callback) {
   await localforage.setItem(attr, value, callback);
@@ -6,7 +7,9 @@ async function set(attr, value, callback) {
 }
 
 async function get(attr, callback) {
-  return await localforage.getItem(attr, callback);
+  return (await localforage.getItem(attr, callback))
+    ? localforage.getItem(attr, callback)
+    : [];
 }
 
 async function getIndex(attr, func) {
@@ -19,14 +22,11 @@ async function getAll(callback) {
   let tasks = await get("tasks");
   let done = await get("done");
   let queue = await get("queue");
-  let cycles = await get("cycles");
 
   if (callback) {
-    Promise.all([tasks, done, queue, cycles]).then(
-      callback({ tasks, queue, done, cycles })
-    );
+    Promise.all([tasks, done, queue]).then(callback({ tasks, queue, done }));
   }
-  return { tasks, done, queue, cycles };
+  return { tasks, done, queue };
 }
 
 async function removeItem(attr, func) {
@@ -41,12 +41,12 @@ async function removeItem(attr, func) {
 
 async function addAtBeginning(attr, item) {
   let array = await get(attr);
-  await localforage.setItem(attr, [item, ...array]);
+  await localforage.setItem(attr, array ? [item, ...array] : [item]);
 }
 
 async function addAtEnd(attr, item) {
   let array = await get(attr);
-  await localforage.setItem(attr, [...array, item]);
+  await localforage.setItem(attr, array ? [...array, item] : [item]);
 }
 
 async function updateItem(attr, item, func) {
@@ -70,6 +70,50 @@ async function reOrder(queue, startIndex, endIndex) {
   return null;
 }
 
+async function log() {
+  let data = await get("chartData");
+  if (data && data.length <= 0) {
+    await set("chartData", chartData);
+    data = chartData;
+  }
+  let tasks = data.tasks;
+  let day = new Date().getDay();
+  if (tasks[7].day !== day) {
+    let diff = tasks[7].day;
+    while (diff++ % 7 !== day) {
+      diff = diff % 7;
+      tasks.splice(0, 1);
+      tasks.push({ day: diff, value: 0 });
+      data.cycles.splice(0, 1);
+      data.cycles.push({ day: diff, value: 0 });
+    }
+    return await set("chartData", data);
+  }
+}
+
+async function logTask() {
+  await log();
+  let data = await get("chartData");
+  let tasks = data.tasks;
+  tasks[7].value += 1;
+
+  return await set("chartData", data);
+}
+
+async function logCycle() {
+  await log();
+  let data = await get("chartData");
+  let cycles = data.cycles;
+  cycles[7].value += 1;
+
+  return await set("chartData", data);
+}
+
+async function getChartData() {
+  await log();
+  return await localforage.getItem("chartData");
+}
+
 export {
   set,
   get,
@@ -79,5 +123,9 @@ export {
   addAtBeginning,
   addAtEnd,
   updateItem,
-  reOrder
+  reOrder,
+  log,
+  logTask,
+  logCycle,
+  getChartData
 };
